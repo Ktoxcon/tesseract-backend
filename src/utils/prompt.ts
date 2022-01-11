4135e3fa3e304a23d0bc3bbcc833c316316b8cca
Ktoxcon <kennethtoxro@gmail.com>
2022-01-10 22:09:48 -0600

    feat(utils/prompt): adding console ANSI colors

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,10 +1,19 @@
 import * as Figlet from "figlet";
 
+/**
+ * Special ANSI escape characters used to
+ * show colors in the console.
+ */
+const CONSOLE_COLORS = {
+  reset: "\u001b[0m",
+  green: "\u001b[32m",
+};
+
 export function generateSeparator(symbol: string, times: number) {
   return String(symbol).repeat(times);
 }
 
-export function displayServerRunningMessage(port: any) {
+export function displayServerRunningMessage(port: number) {
   Figlet("Server Running", (error, message) => {
     if (error) console.log(`Server running on port:${port}`);
 
@@ -12,7 +21,9 @@ export function displayServerRunningMessage(port: any) {
     const paddingLarge = generateSeparator(" ", 15);
 
     console.clear();
-    console.log(`${separator}\n${message}\n${separator}\n`);
+    console.log(`${separator}\n`);
+    console.log(`${CONSOLE_COLORS.green}${message}${CONSOLE_COLORS.reset}\n`);
+    console.log(`${separator}\n`);
     console.log(`${paddingLarge} The server is now running on port: ${port}`);
     console.log(`${paddingLarge}\t\t   Happy hacking :)\n\n${separator}`);
   });
