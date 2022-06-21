e66aa28d878e62c6f6fabf2a56df328313de8593
Kenneth Toxcon <kennethtoxro@gmail.com>
2022-06-21 17:19:55 -0600

    fix: adding cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Express = require("express");
 const RequestHandler = require("./handlers/todos");
+const cors = require("cors");
 
 const { initializeDB } = require("./db");
 const {
@@ -12,6 +13,8 @@ const App = Express();
 
 App.use(Express.json());
 App.use(Express.urlencoded({ extended: false }));
+
+App.use(cors());
 App.use("/v1", RequestHandler);
 
 App.listen(appPort, () => {
